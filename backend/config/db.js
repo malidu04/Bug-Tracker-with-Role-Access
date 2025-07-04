bceaf0a0a4660775fc798ddeac4b87ff271443b9
malidu04 <malidupahasara04@gmail.com>
2025-07-04 22:23:09 +0530

    config db file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
+const colors = require('colors');
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
     process.exit(1);
   }
 };
